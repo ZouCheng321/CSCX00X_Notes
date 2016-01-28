a2119202af9eade0155f9c904420a9a8410534d0
tavimori <tavimori@gmail.com>
2016-01-28 23:33:36 +0800

    Test

--- Pyramid.py
+++ Pyramid.py
@@ -1,3 +1,5 @@
-baseWidth = 300
+baseWidth = 100
 for i in range(baseWidth-1):
-    print(' '*(baseWidth-1-i),'p'*(2*i-1))
+    print(' '*(baseWidth-1-i),'*'*(2*i-1))
+    for j in range(1000000):
+        1+1
