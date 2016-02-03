6022f410ee3e96588cc80f2abaaaa72e5db50356
tavimori <tavimori@gmail.com>
2016-02-03 19:51:25 +0800

    Test for github

---
+++ Assignment1/q6.py
@@ -0,0 +1,13 @@
+# Question 6 (25% of this assignment): Given a function f(x), and a real interval [a, b], the numerical integration of
+#  f(x) over interval [a, b] can be calculated as:
+
+# In equation (1), n represents the number of sub-intervals into which the interval [a, b] will be divided; and it
+#  controls the accuracy of numerical integration.
+
+# Write a program to allow the user to specify a trigonometric function f (f can only be sin, cos or tan), and input
+#  the interval end points a, b and number of sub-intervals n. Your program should then calculate the numerical
+#  integration of f over [a, b] using equation (1), and output the result. Your program should be robust enough to
+#  handle possible improper inputs (e.g. the user inputs a floating point number as n).
+# Python has built-in trigonometric functions. To call them, use the following statement in your program to import
+#  them from the math package:
+# You can then invoke the trigonometric functions like the following examples:
