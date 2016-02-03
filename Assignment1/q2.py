c80d4b73e9a20daba8c31192ce8b0d7da61f4550
tavimori <tavimori@gmail.com>
2016-02-03 18:20:22 +0800

    Add Assignment#1 Question#4#5

--- Assignment1/q2.py
+++ Assignment1/q2.py
@@ -1,3 +1,9 @@
+# Question 2 (15% of this assignment): Write a program that prompts the user to enter an integer and displays the
+#  number in reverse order. Here is a sample run:
+
+# Answers given by tavimori
+
+
 while True:
     integerEnterByUser = input('Enter an integer:')
     try:
