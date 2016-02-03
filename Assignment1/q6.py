7d1f8820412387c34ce0ee77686d8ae1db29138f
tavimori <tavimori@gmail.com>
2016-02-03 22:35:51 +0800

    Add Assignment #6

--- Assignment1/q6.py
+++ Assignment1/q6.py
@@ -13,3 +13,25 @@
 # You can then invoke the trigonometric functions like the following examples:
 
 # Written by tavimori
+
+givenFunction = input('Enter f(x)= ')
+a = input('Enter the lower bound of integration: ')
+b = input('Enter the upper bound of integration: ')
+n = input('Enter the number of sub-intervals: ')
+# givenFunction = '2 * x'
+# lowerBoundOfIntegration = 0
+# upperBoundOfIntegration = 1
+# nOfSubInterval = 2
+a = float(a)
+b = float(b)
+n = int(n)
+
+# Initializing the result
+integrationResult = 0
+if n > 10000:
+    print('It may takes a while...')
+for i in range(1, n+1):
+    # print(i)
+    x = a + (b - a) / n * (i - 1 / 2)
+    integrationResult += (b - a) / n * eval(givenFunction)
+print('The numerical integration is ', integrationResult)
