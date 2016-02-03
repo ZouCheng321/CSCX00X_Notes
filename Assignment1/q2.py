ca0ec5851118beb2343d7e93044a955cdfd3f5d8
tavimori <tavimori@gmail.com>
2016-02-03 18:49:51 +0800

    Test for github

--- Assignment1/q2.py
+++ Assignment1/q2.py
@@ -1,6 +1,7 @@
 # Question 2 (15% of this assignment): Write a program that prompts the user to enter an integer and displays the
 #  number in reverse order. Here is a sample run:
 
+
 # Answers given by tavimori
 
 
