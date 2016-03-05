001bc3a680894296d5ff7ac5c8e4f175a3e5198f
tavimori <tavimori@gmail.com>
2016-03-05 21:34:14 +0800

    Assignment3! Improve compatibility. finish calculation

--- CSC1002/Assignment3/calcaera.py
+++ CSC1002/Assignment3/calcaera.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+TBD:
+y = -x + 1
+y = 1
+"""
 
-"""
-lalalalala
-"""
+
+def debug(execute):
+    """
+    Debug用的东西
+    :param execute:
+    :return:
+    """
+    global isDebug
+    if isDebug:
+        print(execute)
+    else:
+        pass
 
 
 def array_plus(list1, list2):
 
     """
+    就是矩阵加法啦~~
     :param list1:
     :param list2:
     :return:
@@ -24,6 +39,7 @@ def array_plus(list1, list2):
 def array_minus(list1, list2):
 
     """
+    矩阵减法~~
     :param list1:
     :param list2:
     :return:
@@ -38,7 +54,7 @@ def array_minus(list1, list2):
 def array_multi(list1, list2):
 
     """
-
+    矩阵乘法好伐
     :param list1:
     :param list2:
     :return:
@@ -57,7 +73,7 @@ def array_multi(list1, list2):
 def array_divide(list1, list2):
 
     """
-
+    还没打算用的
     :param list1:
     :param list2:
     :return:
@@ -69,7 +85,7 @@ def array_divide(list1, list2):
 def expr_extract(expr_extracted_from):
 
     """
-
+    把表达式整理成链表形式,第一个值表示数据的类型, True为运算符, 否则是字符串
     :param expr_extracted_from:
     :return:
     """
@@ -78,18 +94,29 @@ def expr_extract(expr_extracted_from):
     expr_extract_unit = ''
     expr_extract_result = []
     for i in expr_extracted_from:
-        if i not in '+-*/=\n':
-            if i is not ' ':
-                expr_extract_unit += i
-        else:
-            expr_extract_result.append([True, expr_extract_unit])
+        if i in '+-*/=\n':
+            if i == '-' and len(expr_extract_unit) == 0:
+                expr_extract_result.append([True, '0'])
+            if len(expr_extract_unit) != 0:
+                expr_extract_result.append([True, expr_extract_unit])
             if i is not '\n':
                 expr_extract_result.append([False, i])
+            # else:
+            #     expr_extract_result.append(False, '+')
+            #     expr_extract_result.append(True, '0')
             expr_extract_unit = ''
+        else:
+            if i is not ' ':
+                expr_extract_unit += i
     return expr_extract_result
 
 
 def term_unify(term_to_be_unified):
+    """
+    往简写的一项里面补点运算符啦
+    :param term_to_be_unified:
+    :return:
+    """
     if term_to_be_unified.isnumeric():
         return term_to_be_unified
     elif term_to_be_unified.isalpha():
@@ -101,12 +128,22 @@ def term_unify(term_to_be_unified):
 
 
 def expr_unify(expr_to_be_unified):
+    """
+    对整个式子补充运算符
+    :param expr_to_be_unified:
+    :return:
+    """
     for i in [j for j in range(len(expr_to_be_unified)) if expr_to_be_unified[j][0]]:
         expr_to_be_unified[i][1] = term_unify(expr_to_be_unified[i][1])
     return expr_to_be_unified
 
 
 def expr_variable_to_be_array(list1):
+    """
+    将式中变量归档存储,并用矩阵替换
+    :param list1:
+    :return:
+    """
     variable_dictionary = {'const': 0}
     index = 1
     for i in range(len(list1)):
@@ -116,7 +153,7 @@ def expr_variable_to_be_array(list1):
                 variable_dictionary[term] = index
                 index += 1
     index -= 1
-    print(variable_dictionary)
+    # print(variable_dictionary)
     # blank_list = [0]*(index + 1)
     # print(blank_list)
     for i in range(len(list1)):
@@ -136,7 +173,7 @@ def expr_variable_to_be_array(list1):
 def expr_calc(list1):
 
     """
-
+    进行化简运算啦,先算乘法除法再算加法减法
     :param list1:
     :return:
     """
@@ -164,36 +201,149 @@ def expr_calc(list1):
                 del list1[i - 1]
                 i -= 1
             elif list1[i][1] is '-':
+                # if i == 0 or list1[i - 1][0] == False:
+                #     list1.insert(i, [True, '0'])
+                #     continue
                 list1[i] = [True, array_minus(list1[i - 1][1], list1[i + 1][1])]
                 del list1[i + 1]
                 del list1[i - 1]
                 i -= 1
         i += 1
     i = 0
-    print(list1)
+    # print(list1)
     while i < len(list1):
         if not list1[i][0]:
             if list1[i][1] is '=':
                 return array_minus(list1[i - 1][1], list1[i + 1][1])
         i += 1
 
+
+def deal_with_input_line(input_line):
+    debug('[DEBUG]: Debug mode is on...')
+    debug('[DEBUG]: Raw input is: \n\t\'{}\''.format(input_line))
+    input_line = expr_extract(input_line)
+    debug('[DEBUG]: Extracted input is: \n\t\'{}\''.format(input_line))
+    input_line = expr_unify(input_line)
+    input_line = ''.join([input_line[i][1] for i in range(len(input_line))])
+    input_line = expr_extract(input_line)
+    debug('[DEBUG]: Extracted input(unified) is \n\t\'{}\''.format(input_line))
+    input_line, line_dictionary = expr_variable_to_be_array(input_line)
+    input_line = expr_calc(input_line)
+    debug('[DEBUG]: Abstracted line is {}, \n\twith dictionary {}'.format(input_line, line_dictionary))
+    return input_line, line_dictionary
+
+
+def intersection_point_find(line1, line2):
+    """
+
+    :param line1:
+    :param line2:
+    :return:
+    """
+    a1, b1, c1 = line1
+    a2, b2, c2 = line2
+    if b1 == 0 or b2 == 0:
+        if b1 == b2 == 0:
+            return 'No intersection'
+        elif b1 == 0:
+            x = - c1 / a1
+            y = (- c2 - a2 * x) / b2
+            return x, y
+        elif b2 == 0:
+            x = - c2 / a2
+            y = (- c1 - a1 * x) / b1
+            return x, y
+    else:
+        a1, b1, c1 = (- a1 / b1, - 1, - c1 / b1)
+        a2, b2, c2 = (a2 / b2, 1, c2 / b2)
+        if a1 + a2 == 0:
+            if c1 + c2 == 0:
+                return 'There the same line'
+            else:
+                return 'No intersection'
+        else:
+            x = - (c1 + c2) / (a1 + a2)
+            y = (- c1 - a1 * x) / b1
+            return x, y
+
+
+def calc_magnitude(vector1):
+    from math import sqrt
+    return sqrt(vector1[0] * vector1[0] + vector1[1] * vector1[1])
+
+
+def calc_side_length(point1, point2, point3):
+
+    if point1 == point2 == point3:
+        return False
+    return (calc_magnitude(array_minus(point1, point2)),
+            calc_magnitude(array_minus(point2, point3)),
+            calc_magnitude(array_minus(point1, point3)))
+
+
+def calc_area(length1, length2, length3):
+    p = (length1 + length2 + length3) / 2
+    from math import sqrt
+    return sqrt(p * (p - length1) * (p - length2) * (p - length3))
+
 while True:
-    a = input('Any expr:').lower()
-    # a = '2x= 5y+1'
-    b = expr_extract(a)
-    b = expr_unify(b)
-    b = ''.join([b[i][1] for i in range(len(b))])
-    print(b)
-    b = expr_extract(b)
-    b, dictionary = expr_variable_to_be_array(b)
-    print(b)
-    b = expr_calc(b)
-    print(b)
-    for i in dictionary:
-        # print(i, '=', b[dictionary[i]])
-        pass
-    print('The input is simplified as ',
-          b[dictionary['x']], 'x+',
-          b[dictionary['y']], 'y+',
-          b[dictionary['const']], '=0',
-          sep='')
+    isDebug = True
+    # a = input('Any expr:').lower()
+    # # a = '2x= 5y+1'
+    # b = expr_extract(a)
+    # b = expr_unify(b)
+    # b = ''.join([b[i][1] for i in range(len(b))])
+    # print(b)
+    # b = expr_extract(b)
+    # b, dictionary = expr_variable_to_be_array(b)
+    # print(b)
+    # b = expr_calc(b)
+    # print(b)
+    # for i in dictionary:
+    #     # print(i, '=', b[dictionary[i]])
+    #     pass
+    # print('The input is simplified as ({})x+({})y+{}=0'.format(b[dictionary['x']],
+    #                                                            b[dictionary['y']],
+    #                                                            b[dictionary['const']]))
+    # 
+    line = [0, 0, 0]
+    line_dic = [0, 0, 0]
+
+
+    def if_exist(cmd):
+        try:
+            return eval(cmd)
+        except:
+            return 0
+
+    for i in range(3):
+        line[i] = input('line{}'.format(i))
+        line[i], line_dic[i] = deal_with_input_line(line[i])
+        print('The input is simplified as ({})x+({})y+{}=0'.format(if_exist("line[i][line_dic[i]['x']]"),
+                                                                   if_exist("line[i][line_dic[i]['y']]"),
+                                                                   if_exist("line[i][line_dic[i]['const']]")
+                                                                   )
+              )
+
+        line[i] = (if_exist("line[i][line_dic[i]['x']]"),
+                   if_exist("line[i][line_dic[i]['y']]"),
+                   if_exist("line[i][line_dic[i]['const']]")
+                   )
+        print(line[i])
+    # for i in range(3):
+    #
+    # line1 = input('line 1:')
+    # line1, line1_dic = deal_with_input_line(line1)
+    # line1 = (line1[line1_dic['x']], line1[line1_dic['y']], line1[line1_dic['const']])
+    # line2 = input('line 2:')
+    # line2, line2_dic = deal_with_input_line(line2)
+    # line2 = (line2[line2_dic['x']], line2[line2_dic['y']], line2[line2_dic['const']])
+    # line3 = input('line 3:')
+    # line3, line3_dic = deal_with_input_line(line3)
+    # line3 = (line3[line3_dic['x']], line3[line3_dic['y']], line3[line3_dic['const']])
+    p1 = list(intersection_point_find(line[0], line[1]))
+    p2 = list(intersection_point_find(line[1], line[2]))
+    p3 = list(intersection_point_find(line[0], line[2]))
+    l1, l2, l3 = calc_side_length(p1, p2, p3)
+    area = calc_area(l1, l2, l3)
+    print('面积估摸着大概是', area)
